2d1d4e9be2673452b6037ba9c0c6c592d3e13f72
J0bbie <job.vanriet@tno.nl>
2014-04-09 14:51:50 +0200

    Small update on SIDER2

--- writeData2DB/readSIDER2.py
+++ writeData2DB/readSIDER2.py
@@ -1,4 +1,4 @@
-# Author(s):    Job van Riet
+# Author(s):    Job van Riet + Data from SIDER2/ChEMBL
 # Date of  creation:    27-3-14
 # Date of modification:    Initial version
 # Version:    1.0
@@ -6,29 +6,48 @@
 # Known bugs:    None Known
 # Function:
 # This script can read the SIDER2 files and upload the data to the DB
+# It uploads the contents into a schema designed for SIDER2 data, making use of a compound DB to store the compound and a medDRA database to store the medDRA terms
+# The following files are used: label_mapping, meddra_adverse_effects and meddra_freq_parsed files from SIDER2 in .tsv format.
 
 #Import command line parser
-from optparse import OptionParser
+import argparse
 
 #Import os options (read dir/extensions)
 import os
 
-# Import user specified settings
-import writeData2DB.settings as s
+# Import user specified settings for connecting to the DB
+import settings as s
 
 # Import MySQL functions
 import pymysql
 
 # Get the command line parameters
-parser = OptionParser()
-parser.add_option("-f", "--inputDirectory", dest="inputDirectory", default=os.path.dirname(os.path.realpath(__file__)),
-                  help="Give the input directory containing ALL the files from SIDER2 in .tsv format.")
+
+parser = argparse.ArgumentParser(description='Write SIDER(2) data into the DB')
+parser.add_argument("-i", "--inputDirectory", dest="inputDirectory", default=os.path.dirname(os.path.realpath(__file__)),
+                  help="Give the input directory containing the label_mapping, meddra_adverse_effects and meddra_freq_parsed files from SIDER2 in .tsv format.")
+
+# If the files has different names, supply them with these parameters
+parser.add_argument("-l", "--labelMapping", dest="inputDirectory", default="label_mapping.tsv",
+                  help="Give file name of the label_mapping.tsv if different.")
+
+parser.add_argument("-a", "--adverseEffect", dest="adverseEffect", default="meddra_adverse_effects.tsv",
+                  help="Give file name of the meddra_adverse_effects.tsv if different.")
+
+parser.add_argument("-f", "--adverseFreq", dest="adverseFreq", default="meddra_freq_parsed.tsv",
+                  help="Give file name of the meddra_freq_parsed.tsv if different.")
+
+# Add a mutually exclusive group to overwrite/append/skip data that is already found in the SIDER2 DB
+CRUDGroup = parser.add_mutually_exclusive_group()
+CRUDGroup.add_argument('--overwrite', action='store_true')
+CRUDGroup.add_argument('--skip', action='store_true')
+CRUDGroup.add_argument('--append', action='store_true')
 
 #Parse the options
-(options, args) = parser.parse_args()
-arguments = vars(options)
+args = parser.parse_args()
+arguments = vars(args)
 
-arguments['inputDirectory'] = "C:/Users/rietjv/AppData/Local/My Local Documents/SIDER2/originalFiles/"
+#print(arguments['inputDirectory'])
 
 #######################################################
 #                       Exceptions                    #
@@ -38,33 +57,38 @@ arguments['inputDirectory'] = "C:/Users/rietjv/AppData/Local/My Local Documents/
 class reqFileException(Exception):
     pass
 
-#Make the connection to the DB, using the settings in the setting.py file
-connection = pymysql.connect(host=s.databaseSettings['HOST'],
+#######################################################
+#                       Connections                   #
+#######################################################
+
+# Make the connection to the MYSQL server, using the settings in the setting.py file
+# The SCHEMAS will be used to query to needed database
+connection2SIDER = pymysql.connect(host=s.databaseSettings['HOST'],
                                      port=s.databaseSettings['PORT'],
                                      user=s.databaseSettings['USER'],
                                      passwd=s.databaseSettings['PASSWORD'],
-                                     db=s.databaseSettings['SCHEMA'],
+                                     db=s.databaseSettings['SCHEMASIDER'],
                                      autocommit = False)
 # Create cursor to handle requests
-cursor = connection.cursor()
+cursor= connection.cursor()
 
 #######################################################
 #                   File searching                    #
 #######################################################
 
-#Locate the files in the directory
-for file in os.listdir(arguments['inputDirectory']):
-    if file.endswith(".tsv"):
-        try:
-            #First find the label_mapping.tsv file as this contains the compounds (brand name + generic names and the label identifier used in the other files)
-            if(file == "meddra_adverse_effects.tsv"):
-                print("foo")
-            else:
-                raise reqFileException("Could not locate label_mapping.tsv in directory: "+arguments['inputDirectory'])
-        #If one of the required files could not be located
-        except reqFileException:
-            #Rollback the database to prevent half-complete data inserts
-            connection.rollback()
+#Open the files in the correct order (Label_mapping -> meddra_adverse_effects -> meddra_freq_parsed)
+try:
+    #First find the label_mapping.tsv file as this contains the compounds (brand name + generic names and the label identifier + STITCH ID used in the other files)
+    if(file == "label_mapping.tsv"):
+        filePath = '/'.join(arguments['inputDirectory'], "label_mapping.tsv")
+        #Read the contents 
+        writeLabelMapping(filePath)
+    else:
+        raise reqFileException("Could not locate label_mapping.tsv in directory: "+arguments['inputDirectory'])
+#If one of the required files could not be located
+except reqFileException:
+    #Rollback the database to prevent half-complete data inserts
+    connection.rollback()
 
 #Write the label_mapping.tsv to the DB
 #Contains the compound labels used in the SIDER2 DB
@@ -75,14 +99,19 @@ def writeLabelMapping(filePath):
 def meddraAdverseEffects(filePath):
     print("foo")
     
-#Write the adverse_effects_raw.tsv to the DB
-def adverseEffectsRaw(filePath):
-    print("foo")
-    
-#Write the indications_raw.tsv to the DB
-def indicationsRaw(filePath):
-    print("foo")
-    
 #Write the meddra_freq_parsed.tsv to the DB
 def meddraFreqParsed(filePath):
     print("foo")
+    
+# Try to retrieve the id of the compound from the compoundDB, else create it.
+# Searching can be done with synonyms (brand+generic names etc.)
+def getCreateCompound(connection, cursor, compoundNames):
+    sqlQuery = "SELECT idCompound FROM normdb.tCompound as c LEFT JOIN tCompoundSynonyms ON c.idCompound = tCompoundSynonyms.idCompound WHERE %s OR %s LIMIT 1" %
+    ('name = ')
+
+    pass
+
+# Try to retrieve an existing medDRA term 
+# SIDER2 shows both a LLT and/or PT for each adverse effect
+def getCreateUpdateMedDRA(connection, cursor, medDRAPT, medDRALLT):
+    pass
--- writeData2DB/settings.py
+++ writeData2DB/settings.py
@@ -11,17 +11,24 @@
 #    MySQL Settings    #
 ########################
 
-# Change this to a user/schema with INSERT permissions and the correct fields. (As shown in the ERD)
+# Change this to a user/schema with INSERT permissions and the correct fields.
+# For simplicity sake, the same user is used for all the databases.
 databaseSettings = dict(
     # IP/Hostname of MySQL server
     HOST='127.0.0.1',  # 127.0.0.1
     # Port for connecting to MySQL
     PORT=3306,  # 3306
     # MySQL User
-    USER='jobDiamonds',  # job
+    USER='job',  # job
     # Password for MySQL user
     PASSWORD='jobPass',  # jobPass
-    # Schema which houses the database
-    SCHEMA='adverseDB'  # adverseDB
+    # Schema which houses the database for the SIDER2 data
+    SCHEMASIDER='SIDER2',  # SIDER2
+    # Schema which houses the database for the FAERS data
+    SCHEMAFAERS='FAERS',  # FAERS
+    # Schema which houses the database for the PROTECTADR data
+    SCHEMAPROTECTADR='protectADR',  # protectADR
+    # Schema which houses the database for the medDRA data
+    SCHEMAMEDDRA='medDRA'  # medDRA
 )
 
